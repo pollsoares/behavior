f57123008f2bc435b89b39cb178a4449c9423ca7
soares <soares.si@gmail.com>
2021-02-10 21:27:58 -0300

    inicial commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        echo "Listagem de artigos!";
+
+        $post = Post::find();
+
+        echo "<h1>{$post->title}</h1>";
+        echo "<h2>{$post->subtitle}</h2>";
+        echo "<p>{$post->description}</p>";
+        echo "<hr>";
+        
+
+      
+        //$posts = Post::where('created_at', '>=', date('2020-m-d H:i:s'))->firstOrFail();
+      
+        //foreach($posts as $post){
+        //    echo "<h1>{$post->title}</h1>";
+        //   echo "<h2>{$post->subtitle}</h2>";
+        //    echo "<p>{$post->description}</p>";
+        //    echo "<hr>";
+        //}
+
+       var_dump($post);        
+    
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        //
+    }
+}
